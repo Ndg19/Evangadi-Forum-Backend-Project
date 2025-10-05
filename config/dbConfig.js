48eb187a9f653a343cc2800e3d3ea0ab73f84bc5
Kedamawi <eyasu10@proton.me>
2025-10-04 20:32:59 -0500

    removed port and comments

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,17 +5,7 @@ const dbConnection = mysql.createPool({
   database: process.env.DATABASE,
   host: process.env.HOST,
   password: process.env.PASSWORD,
-  port: 3307,
   connectionLimit: 10
 });
-// db.execute("select 'test' ", (err, result) => {
-//   // console.log("execute - function");
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log(result);
-//   }
-//   // console.log("end - function");
-// });
 
 module.exports = dbConnection.promise();
